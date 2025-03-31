85f7961cd7a72e9c01b3d8d1505db680e630ef37
Murilets <murilobertelli18@gmail.com>
2025-03-31 09:52:15 -0300

    back com sistema de requisicao de filtros

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require("express"); //chamando o express
 const cors = require("cors"); //chamando o cors
-const imoveisService = require("../service/imoveis"); //chamando o cors
+const imoveisService = require("../service/imoveis"); //chamando os imoveis
 
 const app = express(); //varriavel de controle do exprress
 const port = 3220; //variavel de controle definindo a porta
@@ -9,7 +9,55 @@ app.use(cors()); //ativa o cors para fazer requisicao de qualquer origem
 
 //rota para buscar todos os imoveis
 app.get("/imoveis", (req, res) => {
-  const imoveis = imoveisService.list();
+  const { type, minPrice, maxPrice, city, bedroom, bathroom } = req.query;
+  let imoveis = imoveisService.list();
+
+  //fazendo o filtro do imovel
+  if (type) {
+    imoveis = imoveis.filter(
+      (imovel) => imovel.type.toLowerCase() === type.toLowerCase());
+  }
+
+  //filtro por preco minimo
+  if (minPrice) {
+    imoveis = imoveis.filter((imovel) => imovel.price >= Number(minPrice));
+  }
+
+  //filtro por preco maximo
+  if (maxPrice) {
+    imoveis = imoveis.filter((imovel) => imovel.price <= Number(maxPrice));
+    console.log("parametros recebidos", req.query);
+  }
+
+  //filtrando por cidade
+  if (city) {
+    imoveis = imoveis.filter(
+      (imovel) => imovel.city.toLowerCase() === city.toLowerCase()
+    );
+  }
+
+  //filtro por quantidade de quartos
+  if (bedroom) {
+    imoveis = imoveis.filter((imovel) => {
+      const feature = imovel.features.find((f) => f.type === "BEDROOM");
+      return feature && feature.quantity === Number(bedroom);
+    });
+  }
+
+  //filtro por quantidade de banheiros
+  if (bathroom) {
+    imoveis = imoveis.filter((imovel) => {
+      const feature = imovel.features.find((f) => f.type === "BATHROOM");
+      return feature && feature.quantity === Number(bathroom);
+
+    });
+  }
+   //console para debug
+   console.log("imoveis apos  filtro", imoveis)
+
+  if (imoveis.length === 0){
+    return res.status(404).json({message: "Nenhum imovel encontrado"});
+  }
   res.json(imoveis);
 });
 
@@ -26,5 +74,6 @@ app.get("/imoveis/:id", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`); //function pra rodarr na pota 3220
+  console.log(`App listening on port ${port}`); //function pra rodar na porta 3220
 });
+
--- database/data.js
+++ database/data.js
@@ -1,39 +1,165 @@
 const imoveis = [
   {
     id:'e2b68630-1969-4d6b-9721-01efc6a4a47f',
-    type: 'Terreno',
+    type: 'TERRENO',
     price: 5000,
-    address: 'jardim das palmeiras',
-    city: 'Catanduva',
-    state: 'SP',
+    street: 'Rua josoares do balacobaco 521',
+    cep: "15800",
+    city: 'CATANDUVA',
+     estado: 'SP',
     description: 'fodase',
+    features: [
+      {
+        type: "BATHROOM",
+        description: "Banheiros",
+        quantity:0,
+      },
+      {
+        type: "GARAGE",
+        description: "Garagem",
+        quantity: 0,
+      },
+      {
+        type: "LIVINGROOM",
+        description: "Sala",
+        quantity: 0,
+      },
+      {
+        type: "BEDROOM",
+        description: "Quartos",
+        quantity: 0,
+      }
+    ],
+
+    photos: [
+      {
+        img1:'https://images.pexels.com/photos/7986062/pexels-photo-7986062.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        img2: "https://images.pexels.com/photos/7986063/pexels-photo-7986063.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        img3:"https://images.pexels.com/photos/16551361/pexels-photo-16551361/free-photo-of-view-of-a-cropland-and-forest-in-the-background-under-a-cloudy-sky.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"        
+      }
+    ],
   },
   {
     id: 'ba36f342-f673-4d76-997c-2df40aef3492',
-    type: 'Casa',
+    type: 'CASA',
     price: 20000,
-    address: 'Bairro pinheiro',
-    city: 'Catanduva',
-    state: 'SP',
+    street: 'Rua santo antonio 876',
+    cep: "15800",
+    city: 'CATANDUVA',
+     estado: 'SP',
     description: 'fodase',
+
+    features: [
+      {
+      type: "BATHROOM",
+      description: "Banheiros",
+      quantity: 1,
+    },
+    {
+      type: "GARAGE",
+      description: "Garagem",
+      quantity: 1,
+    },
+    {
+      type: "LIVINGROOM",
+      description: "Sala",
+      quantity: 1,
+    },
+    {
+      type: "BEDROOM",
+      description: "Quartos",
+      quantity: 1,
+    }
+  ],
+    photos: [{
+      img1:"https://images.pexels.com/photos/277667/pexels-photo-277667.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      img2:"https://images.pexels.com/photos/3288100/pexels-photo-3288100.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      img3:"https://images.pexels.com/photos/3288104/pexels-photo-3288104.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      img4:"https://images.pexels.com/photos/2635038/pexels-photo-2635038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      img5:"https://images.pexels.com/photos/3797991/pexels-photo-3797991.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+           
+    }
+   ],
   },
   {
     id: 'b1924db1-8439-4b4c-a39c-5bea593ff156',
-    type: 'Apartamento',
+    type: 'APARTAMENTO',
     price: 40000,
-    address: 'Imperial',
-    city: 'Catanduva',
-    state: 'SP',
+    street: 'Rua Zacarias cascalho 85',
+    cep: "15870-000",
+    city: 'CATIGUA',
+     estado: 'SP',
     description: 'fodase',
+
+    features: [{
+      type: "BATHROOM",
+      description: "Banheiros",
+      quantity: 2,
+    },
+    {
+      type: "GARAGE",
+      description: "Garagem",
+      quantity: 1,
+    },
+    {
+      type: "LIVINGROOM",
+      description: "Sala",
+      quantity: 1,
+    },
+    {
+      type: "BEDROOM",
+      description: "Quartos",
+      quantity: 3,
+    }
+  ],
+    photos: [{
+      img1:"https://images.pexels.com/photos/1717272/pexels-photo-1717272.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      img2:"https://images.pexels.com/photos/19776463/pexels-photo-19776463/free-photo-of-spacious-bathroom-with-white-bathtub-and-beige-walls.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      img3:"https://images.pexels.com/photos/1643384/pexels-photo-1643384.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      img4:"https://images.pexels.com/photos/439227/pexels-photo-439227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      img5:"https://images.pexels.com/photos/533157/pexels-photo-533157.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+           
+    }
+  ],
+
   },
   {
     id: '4d93dde1-49c5-4c27-8f1d-e2594e782963',
-    type: 'Terreno',
+    type: 'CASA',
     price: 100000,
-    address: 'Seila fodase',
-    city: 'Catigua',
-    state: 'SP',
+    street: 'AV. marciano alagoas',
+    cep: "15880-000",
+    city: 'TABAPUA',
+     estado: 'SP',
     description: 'fodase',
+
+    features: [{
+      type: "BATHROOM",
+      description: "Banheiros",
+      quantity: 3,
+    },
+    {
+      type: "GARAGE",
+      description: "Garagem",
+      quantity: 2,
+    },
+    {
+      type: "LIVINGROOM",
+      description: "Sala",
+      quantity: 2,
+    },
+    {
+      type: "BEDROOM",
+      description: "Quartos",
+      quantity: 2,
+    }
+  ],
+    photos: [{
+      img1:"https://images.pexels.com/photos/4030036/pexels-photo-4030036.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      img2:"https://images.pexels.com/photos/2121120/pexels-photo-2121120.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      img3:"https://images.pexels.com/photos/2724748/pexels-photo-2724748.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      img4:"https://images.pexels.com/photos/2631746/pexels-photo-2631746.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    }]
   },
 ];
 
